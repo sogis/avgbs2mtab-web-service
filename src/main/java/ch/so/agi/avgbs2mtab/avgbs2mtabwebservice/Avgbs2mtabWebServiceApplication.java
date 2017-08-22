714d2bbb25b6d786996ecf5dcaf1f03daaaa892d
Stefan Ziegler <edi.gonzales@gmail.com>
2017-08-22 19:46:27 +0200

    project skeleton

---
+++ src/main/java/ch/so/agi/avgbs2mtab/avgbs2mtabwebservice/Avgbs2mtabWebServiceApplication.java
@@ -0,0 +1,12 @@
+package ch.so.agi.avgbs2mtab.avgbs2mtabwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avgbs2mtabWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avgbs2mtabWebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/so/agi/avgbs2mtab/avgbs2mtabwebservice/Avgbs2mtabWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.so.agi.avgbs2mtab.avgbs2mtabwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Avgbs2mtabWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
